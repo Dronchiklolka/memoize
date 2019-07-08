035b9b3a5346bc5201ae85b9de11e30aa1a3ad3f
Andrei Boikov <andrei_boikov@epam.com>
2019-07-08 13:27:23 +0300

    Passes 12 tests

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,16 @@
-export function memoize() {
-    // TODO
+export function memoize(f) {
+    if (typeof f === 'undefined' || typeof f !== 'function') {
+        return null;
+    }
+    const cache = {};
+    function memoized(...args) {
+        const key = JSON.stringify(args);
+        if (key in cache) {
+            return cache[key];
+        }
+        const result = f.apply(this, args);
+        cache[key] = result;
+        return result;
+    }
+    return memoized;
 }
